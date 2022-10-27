6df122db85a8884a4db6b7a6fd4e2d81aef339c9
Coooookies <admin@mitay.net>
2022-10-27 10:37:11 +0800

    feat: 修改项目基础结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import communityView from "../views/community.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      redirect: "/community",
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/community",
+      name: "community",
+      component: communityView,
+    },
+  ],
+});
 
-export default router
+export default router;
