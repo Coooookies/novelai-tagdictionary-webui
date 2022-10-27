d17bf6ceb09dfd8776919ac2dbf35e1f6f2d3e55
Coooookies <admin@mitay.net>
2022-10-28 00:32:23 +0800

    feat: 修改图标组件

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-17 22:40:28
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-17 22:44:23
+ * @FilePath: \novel\src\icon\svg-icon.vue
+ * @Description: 
+-->
+<script setup lang="ts">
+const props = defineProps({
+  iconName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <svg><use :xlink:href="`#icon-${props.iconName}`"></use></svg>
+</template>
---
+++ src/components/icon/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-17 22:40:32
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-17 22:41:12
+ * @FilePath: \novel\src\icon\index.ts
+ * @Description:
+ */
+export { default as NIcon } from "./icon.vue";
+export { default as NIconBg } from "./icon-bg.vue";
