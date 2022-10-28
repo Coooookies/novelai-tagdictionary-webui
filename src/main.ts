ffc7955d70b09279844974bd946b7539f8f52276
Coooookies <admin@mitay.net>
2022-10-29 00:48:56 +0800

    add: 添加Fastclick

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-27 00:46:18
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-28 22:44:56
+ * @FilePath: \novelai-tagdictionary-webui\src\main.ts
+ * @Description: 
+ */
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FastClick as fastclick } from "fastclick";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,3 +21,4 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+fastclick.attach(document.body);
