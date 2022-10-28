617f56e5ca87e63d29e7c935fb0c212bf909af08
Coooookies <admin@mitay.net>
2022-10-29 00:51:15 +0800

    feat: router目录修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,13 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-27 00:46:18
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-28 20:58:15
+ * @FilePath: \novelai-tagdictionary-webui\src\router\index.ts
+ * @Description:
+ */
 import { createRouter, createWebHistory } from "vue-router";
-import communityView from "../views/community.vue";
+import communityView from "./routes/community.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/router/routes/community.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-27 11:41:03
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-28 21:45:11
+ * @FilePath: \novelai-tagdictionary-webui\src\router\routes\community.vue
+ * @Description: 
+-->
+<script setup lang="ts"></script>
+<template></template>
+<style lang="scss" scoped></style>
