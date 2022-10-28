10081c8a0d68b11ad03cd5718ac7fba78f5a26bd
Coooookies <admin@mitay.net>
2022-10-29 00:50:28 +0800

    feat: select组件

---
+++ src/components/select/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-28 20:46:06
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-28 20:48:04
+ * @FilePath: \novelai-tagdictionary-webui\src\components\select\index.ts
+ * @Description: 
+ */
+export { default as NSelectMarker } from "./select-marker.vue";
---
+++ src/components/select/select-marker.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    width="8"
+    height="6"
+    viewBox="0 0 8 6"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="3.6"
+      y="3.45001"
+      width="3.81838"
+      height="1.27279"
+      transform="rotate(-45 3.6 3.45001)"
+    />
+    <rect
+      x="0.9"
+      y="0.75"
+      width="5.09117"
+      height="1.27279"
+      transform="rotate(45 0.9 0.75)"
+    />
+  </svg>
+</template>
