0c61859de8c44cf1997a8b4f34f2f2a6fc2c1e40
Coooookies <admin@mitay.net>
2022-10-29 18:23:19 +0800

    add: input实现

---
+++ src/components/input/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-29 17:06:00
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-29 17:06:41
+ * @FilePath: \novelai-tagdictionary-webui\src\components\input\index.ts
+ * @Description: 
+ */
+export { default as NInput } from "./input.vue";
---
+++ src/components/input/input.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { NIcon } from "../icon";
+
+const emits = defineEmits<{
+  (e: "update:value", value: string): void;
+}>();
+
+const props = defineProps({
+  type: String,
+  disabled: Boolean,
+  placeholder: String,
+  value: String,
+  iconName: String,
+});
+
+const inputHandler = (event: Event) => {
+  emits("update:value", (event.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+  <div class="n-input">
+    <n-icon
+      class="n-input__icon"
+      :icon-name="props.iconName"
+      v-if="props.iconName"
+    />
+    <input
+      class="n-input__input"
+      :type="props.type"
+      :value="props.value"
+      :placeholder="props.placeholder"
+      :disabled="props.disabled"
+      @input="inputHandler"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
